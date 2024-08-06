4af8775741299135f7bfeffe36a6ba51923bd8aa
steakbutter <carlosgzzperales@gmail.com>
2024-08-05 21:37:34 -0600

    Added the header to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
-import Header from './components/UI/Header';
+import Header from './components/Header';
+import Footer from './components/UI/Footer';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Header />
-      <Nav />
       <Outlet />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
 // Bringing in the required import from 'react-router-dom'
 import { Link } from 'react-router-dom';
-import Navbar from './UI/Navbar';
+import Header from './UI/Navbar';
 
-export default function Nav() {
+export default function HEader() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
-    <Navbar
-      links={[
-        <Link key={1} className="nav-link text-light" to="/">
+    <>
+    <h1>This is a header </h1>
+    <Header
+    links={[
+      <Link key={1} className="nav-link text-light" to="/">
           Home
         </Link>,
         <Link key={2} className="nav-link text-light" to="/about">
           About Us
         </Link>,
       ]}
-    />
-  );
-}
+      />
+    </>
+    );
+  }
