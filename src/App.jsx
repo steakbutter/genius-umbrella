a3bbb737d0753d96182d78e6d2766a6773418dfe
steakbutter <carlosgzzperales@gmail.com>
2024-08-05 21:29:35 -0600

    added header.jsx file and exported it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+import Header from './components/UI/Header';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+      <Header />
       <Nav />
       <Outlet />
     </>
---
+++ src/components/UI/Header.jsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    return (
+
+<h1> this is the header </h1>
+    )
+}
