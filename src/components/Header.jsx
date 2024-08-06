4ff6845b025e103599ef72b4b4cc6344e947a659
steakbutter <carlosgzzperales@gmail.com>
2024-08-05 21:48:02 -0600

    Added the links to the navbar to the different pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,17 @@ export default function HEader() {
       <Link key={1} className="nav-link text-light" to="/">
           Home
         </Link>,
-        <Link key={2} className="nav-link text-light" to="/about">
-          About Us
+        <Link key={2} className="nav-link text-light" to="/contact">
+        Contact
+      </Link>,
+      <Link key={3} className="nav-link text-light" to="/resume">
+      Resume
+    </Link>,
+    <Link key={4} className="nav-link text-light" to="/portfolio">
+    Portfolio
+  </Link>,
+        <Link key={5} className="nav-link text-light" to="/about">
+          About Me
         </Link>,
       ]}
       />
