6a8c8819c83af1b6d27392df65ad9cc5c42368f7
steakbutter <carlosgzzperales@gmail.com>
2024-08-12 15:15:57 -0600

    Added links to deployed appliations to images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,9 @@
 export default function Portfolio() {
     return (
       <div>
-        <a href=""><img src="images/employeeSQL.png" alt="employee tracker app" /></a>
-        <a href=""><img src="images/finance planner.png" alt="financial planner app" /></a>
-        <a href=""><img src="images/MetaLife Week (1).png" alt="Portfolio website" /></a>
+        <a href="https://drive.google.com/file/d/1lW0zfDE50wVJRXyGj9PvnoGBq0t0OpQg/view"><img src="images/employeeSQL.png" alt="employee tracker app" /></a>
+        <a href="https://symmetrical-couscous-1.onrender.com/"><img src="images/finance planner.png" alt="financial planner app" /></a>
+        <a href="https://steakbutter.github.io/Portfolio-Carlos/"><img src="images/MetaLife Week (1).png" alt="Portfolio website" /></a>
         <a href="https://drive.google.com/file/d/1yDQYrnYpGTg3IAshQG_b0R6Il2c6zSQX/view"><img src="images/networkAPI.png" alt="network API app" /></a>
         <a href="https://drive.google.com/file/d/1Dx1T_YYvFaDWK9_7qJ0Z9a3BD4rvtv5S/view"><img src="images/sql back end.png" alt="SQL back end app" /></a>
         <a href="https://steakbutter.github.io/Rubber-Butter/"><img src="images/Cat Quest (1).png" alt="Website Game" /></a>
