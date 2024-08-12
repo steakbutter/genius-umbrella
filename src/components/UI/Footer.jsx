6a65b96447d297a992800c7f5440ded2045adea6
steakbutter <carlosgzzperales@gmail.com>
2024-08-12 15:09:00 -0600

    Added images to the portfolio page

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
 
-<div>
+<div className="navbar navbar-expand-lg bg-secondary">
     <a href="https://github.com/steakbutter"><img src="images/GithubLogo.png" alt="github logo" /> </a>
     <a href=""><img src="images/linkedinLogo.png" alt="linkedin logo" /></a>   
     <a href=""><img src="images/instagramLogo.png" alt="instagram logo" /></a> 
